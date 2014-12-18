84380d6500292e0c1aa76312de92c1610770e80d
Eric Mertens <emertens@gmail.com>
2014-12-17 21:35:59 -0800

    fiddle with tinytrie

--- src/Data/TinyTrie.hs
+++ src/Data/TinyTrie.hs
@@ -1,8 +1,7 @@
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE DefaultSignatures #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE TypeFamilies #-}
 
 {- |
 
@@ -15,40 +14,43 @@ data Demo = DemoC1 'Int' | DemoC2 'Int' 'Char'  deriving 'Generic'
 instance 'TrieKey' Demo
 @
 
-* Example operations on 'Trie's
-* @ 'view' . 'at' :: 'TrieKey' k => k -> 'Trie' k a -> 'Maybe' a @
-* @ 'set'  . 'at' :: 'TrieKey' k => k ->             'Maybe' a  -> 'Trie' k a -> 'Trie' k a @
-* @ 'over' . 'at' :: 'TrieKey' k => k -> ('Maybe' a -> 'Maybe' a) -> 'Trie' k a -> 'Trie' k a @
-* @ 'toList'    :: 'TrieKey' k => 'Trie' k a -> [a] @
-* @ 'itoList'   :: 'TrieKey' k => 'Trie' k a -> [(k,a)] @
+Example operations on 'Trie's
+
+@
+-- Lookup value from trie
+'view' . 'at' :: 'TrieKey' k => k -> 'Trie' k a -> 'Maybe' a @
+
+-- Insert value into trie
+'set'  . 'at' :: 'TrieKey' k => k ->             'Maybe' a  -> 'Trie' k a -> 'Trie' k a @
+
+-- Alter values stored in trie
+'over' . 'at' :: 'TrieKey' k => k -> ('Maybe' a -> 'Maybe' a) -> 'Trie' k a -> 'Trie' k a @
+
+-- Construct empty trie
+'review' '_Empty' () :: 'TrieKey' k => 'Trie' k a
+@
 -}
 
 module Data.TinyTrie
   (
   -- * Trie data family
     Trie(..)
-  -- * Trie operations
+  -- * Instance implementation details
   , TrieKey(..)
+  -- * Generic implementation details
+  , GTrieKey(..)
+  , GTrie(..)
   ) where
 
 
-
-import Control.Applicative
 import Control.Lens
-import Data.Char (ord,chr)
-import Data.Foldable (Foldable(..), toList)
-import Data.Int
+import Data.Char (ord)
 import Data.IntMap (IntMap)
 import Data.Map (Map)
 import Data.Maybe (isNothing)
-import Data.Monoid (Monoid(..))
-import Data.Semigroup (Semigroup(..))
-import Data.Traversable (fmapDefault, foldMapDefault)
-import Data.Word
 import GHC.Generics
 import qualified Data.IntMap as IntMap
 import qualified Data.Map as Map
-import qualified Data.Foldable as Foldable
 
 
 -- | Keys that support prefix-trie map operations.
@@ -58,36 +60,38 @@ class TrieKey k where
 
   -- | Type of the representation of tries for this key.
   type TrieRep k a
+
+  -- | Half implementation of '_Empty' method
+  trieNull :: Trie k a -> Bool
+
+  -- | Half implementation of '_Empty' method
+  trieEmpty :: Trie k a
+
+  -- | Implementation of 'at' method
+  trieAt :: Functor f => k -> LensLike' f (Trie k a) (Maybe a)
+
+  -- | Implementation of 'show' method
+  trieShow :: Show a => Trie k a -> String
+
+
+  -- Defaults using 'Generic'
   type instance TrieRep k a = GTrie (Rep k) a
 
-
-  -- | Returns 'True' when the 'Trie' contains no values.
-  trieNull :: Trie k a -> Bool
   default trieNull ::
-    (GTrieKey (Rep k) , TrieRep k a ~ GTrie (Rep k) a) =>
-    Trie k a -> Bool
+    (GTrieKey (Rep k), TrieRep k a ~ GTrie (Rep k) a) => Trie k a -> Bool
   trieNull (MkTrie x) = gtrieNull x
 
-
-  -- | Returns a 'Trie' containing no values.
-  trieEmpty :: Trie k a
   default trieEmpty ::
-    ( GTrieKey (Rep k) , TrieRep k a ~ GTrie (Rep k) a) =>
-    Trie k a
-  trieEmpty = genericTrieEmpty
+    (GTrieKey (Rep k), TrieRep k a ~ GTrie (Rep k) a) => Trie k a
+  trieEmpty = MkTrie gtrieEmpty
 
-
-  -- | 'Lens' for visiting elements of the 'Trie'
-  trieAt :: Functor f => k -> LensLike' f (Trie k a) (Maybe a)
   default trieAt ::
     ( GTrieKey (Rep k), Generic k, TrieRep k a ~ GTrie (Rep k) a, Functor f) =>
     k -> LensLike' f (Trie k a) (Maybe a)
   trieAt k f (MkTrie x) = fmap MkTrie (gtrieAt (GHC.Generics.from k) f x)
 
-
-  trieShow :: Show a => Trie k a -> String
   default trieShow ::
-    ( Show a , GTrieKey (Rep k) , TrieRep k a ~ GTrie (Rep k) a) =>
+    (Show a, GTrieKey (Rep k), TrieRep k a ~ GTrie (Rep k) a) =>
     Trie k a -> String
   trieShow (MkTrie x) = gtrieShow x
 
@@ -95,16 +99,10 @@ class TrieKey k where
   {-# INLINE trieEmpty #-}
   {-# INLINE trieAt #-}
 
+
 -- | Effectively associated datatype of tries indexable by keys of type @k@.
 newtype Trie k a = MkTrie (TrieRep k a)
 
--- I don't actually know why this needs to be its own definition, but
--- type checking fails if I inline it.
-genericTrieEmpty ::
-  ( GTrieKey (Rep k) , TrieRep k a ~ GTrie (Rep k) a) =>
-  Trie k a
-genericTrieEmpty = MkTrie gtrieEmpty
-{-# INLINE genericTrieEmpty #-}
 
 ------------------------------------------------------------------------------
 -- Manually derived instances for base types
@@ -156,10 +154,18 @@ instance TrieKey k                         => TrieKey [k]
 -- Generic implementation class
 ------------------------------------------------------------------------------
 
+-- | Generic Trie structures
+data    family   GTrie (f :: * -> *) a
+newtype instance GTrie (M1 i c f) a     = MTrie (GTrie f a)
+data    instance GTrie (f :+: g)  a     = STrie !(GTrie f a) !(GTrie g a)
+newtype instance GTrie (f :*: g)  a     = PTrie (GTrie f (GTrie g a))
+newtype instance GTrie (K1 i k)   a     = KTrie (Trie k a)
+newtype instance GTrie U1         a     = UTrie (Maybe a)
+data    instance GTrie V1         a     = VTrie
+
 -- | TrieKey operations on Generic representations used to provide
 -- the default implementations of tries.
 class GTrieKey f where
-  data GTrie f a
   gtrieAt    :: Functor g => f () -> LensLike' g (GTrie f a) (Maybe a)
   gtrieNull  :: GTrie f a -> Bool
   gtrieEmpty :: GTrie f a
@@ -170,7 +176,6 @@ class GTrieKey f where
 ------------------------------------------------------------------------------
 
 instance GTrieKey f => GTrieKey (M1 i c f) where
-  newtype GTrie (M1 i c f) a    = MTrie (GTrie f a)
   gtrieAt (M1 k) f (MTrie x)    = fmap MTrie (gtrieAt k f x)
   gtrieNull (MTrie x)           = gtrieNull x
   gtrieEmpty                    = MTrie gtrieEmpty
@@ -185,7 +190,6 @@ instance GTrieKey f => GTrieKey (M1 i c f) where
 
 
 instance TrieKey k => GTrieKey (K1 i k) where
-  newtype GTrie (K1 i k) a      = KTrie (Trie k a)
   gtrieAt (K1 k) f (KTrie x)    = fmap KTrie (trieAt k f x)
   gtrieEmpty                    = KTrie trieEmpty
   gtrieNull (KTrie x)           = trieNull x
@@ -199,7 +203,6 @@ instance TrieKey k => GTrieKey (K1 i k) where
 ------------------------------------------------------------------------------
 
 instance (GTrieKey f, GTrieKey g) => GTrieKey (f :*: g) where
-  newtype GTrie (f :*: g) a = PTrie (GTrie f (GTrie g a))
 
   gtrieAt (i :*: j) f (PTrie x)
      = fmap PTrie (gtrieAt i (anon gtrieEmpty gtrieNull (gtrieAt j f)) x)
@@ -218,7 +221,6 @@ instance (GTrieKey f, GTrieKey g) => GTrieKey (f :*: g) where
 
 instance (GTrieKey f, GTrieKey g) => GTrieKey (f :+: g) where
 
-  data GTrie (f :+: g) a        = STrie !(GTrie f a) !(GTrie g a)
   gtrieAt (L1 k) f (STrie x y)  = fmap (`STrie` y) (gtrieAt k f x)
   gtrieAt (R1 k) f (STrie x y)  = fmap (x `STrie`) (gtrieAt k f y)
   gtrieEmpty                    = STrie gtrieEmpty gtrieEmpty
@@ -234,7 +236,6 @@ instance (GTrieKey f, GTrieKey g) => GTrieKey (f :+: g) where
 ------------------------------------------------------------------------------
 
 instance GTrieKey U1 where
-  newtype GTrie U1 a            = UTrie (Maybe a)
   gtrieAt _ f (UTrie x)         = fmap UTrie (f x)
   gtrieEmpty                    = UTrie Nothing
   gtrieNull (UTrie u)           = isNothing u
@@ -248,7 +249,6 @@ instance GTrieKey U1 where
 ------------------------------------------------------------------------------
 
 instance GTrieKey V1 where
-  data GTrie V1 a               = VTrie
   gtrieAt k _ _                 = k `seq` error "GTrieKey.V1: gtrieAt"
   gtrieEmpty                    = VTrie
   gtrieNull _                   = True
@@ -274,3 +274,7 @@ instance TrieKey k => Ixed (Trie k a) where
 instance TrieKey k => At (Trie k a) where
   at = trieAt
   {-# INLINE at #-}
+
+instance TrieKey k => AsEmpty (Trie k a) where
+  _Empty = prism' (const trieEmpty)
+                  (\x -> if trieNull x then Just () else Nothing)
